7d76c54b112a5938deac410f97e7a4ac2ebe24da
victor reuben <130871216+Victorr204@users.noreply.github.com>
2025-03-18 08:43:34 +0100

    my code

---
+++ cryptofy/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-red-300 text-blue-500">
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default App
